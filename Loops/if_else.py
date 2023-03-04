0a7e23ad9eb4a051ea8f5148e651776221e84702
unknown <chandru151998@gmail.com>
2023-03-05 01:10:47 +0530

    [Chandru]Added. Pattern programs

---
+++ Loops/if_else.py
@@ -0,0 +1,53 @@
+# # if statement is used to test a specific condition. If the condition is true, a block of code will be executed
+# def function1(num):
+#     if num != 0:
+#         value = num // 2
+#         print(value)
+#
+#
+# function1(10)
+#
+#
+# # if-else statement is similar to if statement except the fact that, it also provides the block of the code for the
+# # false case of the condition to be checked.
+# # If the condition provided in the if statement is false, then the else statement will be executed
+# def function2(num):
+#     if num % 2 == 0:
+#         print('even')
+#     else:
+#         print('odd')
+#
+#
+# function2(20)
+#
+#
+# # elif statement enables us to check multiple conditions and execute the specific block of statements depending upon
+# # the true condition among them. We can have any number of elif statements in our program depending upon our need
+# def function3(marks):
+#     if 85 < marks <= 100:
+#         print("grade A")
+#     elif 60 < marks <= 85:
+#         print("grade B")
+#     elif 40 < marks <= 60:
+#         print("grade C")
+#     elif 30 < marks <= 40:
+#         print("grade D")
+#     else:
+#         print("fail")
+#
+#
+# function3(58)
+#
+#
+# # Nested if statements enable us to use if-else statement inside an outer if statement
+# def function4(arg):
+#     if isinstance(arg, int):
+#         if arg % 2 == 0:
+#             print('even')
+#         else:
+#             print('odd')
+#     else:
+#         print(f'{arg} is not an integer')
+#
+#
+# function4(5)
--- Pattern Programs/1_program.py
+++ Pattern Programs/1_program.py
@@ -80,3 +80,144 @@ def reverse_rev_num_pattern():
 
 
 # reverse_rev_num_pattern()
+
+def incrementing_nums():
+    rows = int(input("Enter number of rows to be displayed: "))
+    num = 1
+    col = 1
+    for i in range(1, rows + 1):
+        for j in range(1, col + 1):
+            print(num, end=' ')
+            num = num + 1
+        print()
+        col = col + 2
+
+
+# incrementing_nums()
+
+
+def increment_decrement_nums():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(j, end=' ')
+        for k in range(j - 1, 0, -1):
+            print(k, end=' ')
+        print()
+
+
+# increment_decrement_nums()
+
+def space_star():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(1, rows + 1):
+        for j in range(rows, i, -1):
+            print(' ', end=' ')
+        for k in range(1, i + 1):
+            print('*', end=' ')
+        print()
+
+
+# space_star()
+
+
+def rev_space_star():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(rows, 0, -1):
+        for j in range(rows, i, -1):
+            print(' ', end=' ')
+        for k in range(1, i + 1):
+            print('*', end=' ')
+        print()
+
+
+# rev_space_star()
+
+def space_num():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(1, rows + 1):
+        for j in range(rows, i, -1):
+            print(' ', end=' ')
+        for k in range(1, i + 1):
+            print(k, end=' ')
+        print()
+
+
+# space_num()
+
+
+def rev_space_num():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(rows, 0, -1):
+        for j in range(rows, i, -1):
+            print(' ', end=' ')
+        for k in range(i, 0, -1):
+            print(k, end=' ')
+        print()
+
+
+# rev_space_num()
+
+
+def space_similar_num():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(1, rows + 1):
+        for j in range(rows, i, -1):
+            print(' ', end=' ')
+        for k in range(1, i + 1):
+            print(i, end=' ')
+        print()
+
+
+# space_similar_num()
+
+
+def space_similar_rev_num():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(rows, 0, -1):
+        for j in range(rows, i, -1):
+            print(' ', end=' ')
+        for k in range(1, i + 1):
+            print(i, end=' ')
+        print()
+
+
+# space_similar_rev_num()
+
+
+def star_pyramid():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(1, rows + 1):
+        for j in range(1, rows - i + 1):
+            print(end=' ')
+        for j in range(i):
+            print('*', end=' ')
+        print()
+
+
+# star_pyramid()
+
+
+def rev_star_pyramid():
+    rows = int(input("Enter number of rows to be displayed: "))
+    for i in range(rows, 0, -1):
+        for j in range(1, rows - i + 1):
+            print(end=' ')
+        for j in range(i):
+            print('*', end=' ')
+        print()
+
+
+# rev_star_pyramid()
+
+
+def star_diamond():
+    r = int(input("Enter number of rows to be displayed: "))
+    rows = r // 2
+    for i in range(1, rows + 1):
+        print(' ' * (rows - i), '* ' * (i))
+    for j in range(rows - 1, 0, -1):
+        print(' ' * (rows - j), '* ' * (j))
+
+
+star_diamond()
