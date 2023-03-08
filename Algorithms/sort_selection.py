42a85593b0c6404adcbb8fad8ed3b76aa1c7df30
unknown <chandru151998@gmail.com>
2023-03-09 01:04:26 +0530

    [Chandru]Added. Selection sort

--- Algorithms/sort_selection.py
+++ Algorithms/sort_selection.py
@@ -1,12 +1,12 @@
 def selection_sort(arr):
-    for i in range(len(arr) - 1):
+    for i in range(0, len(arr) - 1):
         min_idx = i
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_idx]:
-                min_idx = i
+                min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
 
-data = [-2, 45, 0, 11, -9]
+data = [3, 2, 1]
 selection_sort(data)
 print(data)
