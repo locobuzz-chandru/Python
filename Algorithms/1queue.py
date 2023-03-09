df2fa0feda6a51d39f07a3b637980f7a38b6878e
unknown <chandru151998@gmail.com>
2023-03-09 14:17:28 +0530

    [Chandru]Added. Queue program

--- Algorithms/1queue.py
+++ Algorithms/1queue.py
@@ -1,3 +1,6 @@
+import queue
+
+
 def queue_function1():
     queue_ = []
     for x in range(1, 6):
@@ -18,7 +21,6 @@ def queue_function2():
 
 
 def queue_function3():
-    import queue
     queue_ = queue.Queue()
     for x in range(1, 6):
         queue_.put(x)
@@ -27,7 +29,18 @@ def queue_function3():
     return queue_
 
 
+def priority_queue():
+    queue_ = queue.PriorityQueue()
+    queue_.put(10)
+    queue_.put(30)
+    queue_.put(20)
+    for x in range(3):
+        queue_.get()
+    return queue_
+
+
 if __name__ == '__main__':
     queue_function1()
     queue_function2()
     queue_function3()
+    priority_queue()
